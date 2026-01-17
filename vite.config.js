bcafe9080aa0030f5c25b2682a9b8a2d7426c633
Yoel zajdner <gyozan@Yoels-MacBook-Pro.local>
2026-01-17 23:29:24 +0200

    feat: implement Physics Lanyard Intro with embedded form

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
+    assetsInclude: ['**/*.glb'],
     define: {
         __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
     },
